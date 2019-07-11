df1eef419027e190a8fa39266224d231bf48ee80
DryFear <voronin8945@mail.ru>
2019-07-12 01:15:26 +0300

    Add images of rundom dice's sides instead text

--- app/src/main/java/ru/unfortunately/diceroller/MainActivity.kt
+++ app/src/main/java/ru/unfortunately/diceroller/MainActivity.kt
@@ -8,10 +8,13 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
+        diceImage = findViewById(R.id.dice_image)
         rollButton.setOnClickListener {
             rollDice()
         }
@@ -27,7 +30,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(image)
     }
 }
