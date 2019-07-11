9c1ad6bd1da411b82b091605f1064eb3a52f908c
DryFear <voronin8945@mail.ru>
2019-07-11 21:43:04 +0300

    First commit :3

---
+++ app/src/main/java/ru/unfortunately/diceroller/MainActivity.kt
@@ -0,0 +1,15 @@
+package ru.unfortunately.diceroller
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+
+    }
+}
