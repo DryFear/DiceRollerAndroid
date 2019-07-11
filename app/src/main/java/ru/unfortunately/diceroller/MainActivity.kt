ad408c71a5d28b75668dd17c5d2a84d6eb187ae0
DryFear <voronin8945@mail.ru>
2019-07-11 21:58:26 +0300

    Add randimize

--- app/src/main/java/ru/unfortunately/diceroller/MainActivity.kt
+++ app/src/main/java/ru/unfortunately/diceroller/MainActivity.kt
@@ -3,6 +3,8 @@ package ru.unfortunately.diceroller
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +12,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
 
+    private fun rollDice() {
+        val textView: TextView = findViewById(R.id.result)
+        val rundomNumber: Int = Random.nextInt(6) + 1
+        textView.text = rundomNumber.toString()
     }
 }
